5bed3bf299c235d57477dfa8e058e7be2c406da7
vuyolweth <94593996+vuyolweth@users.noreply.github.com>
2021-11-18 08:23:28 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## <<- operator is used to assign a value to an object in an environment that 
+## is different from the current environment.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function(){x}
+  setInverse <- function(inverse){inv <- inverse}
+  getInverse <- function() {inv}
+  list(set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## the following function calculates the mean of the special "vector" 
+## created with the above function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv) ## Return a matrix that is the inverse of 'x'
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
